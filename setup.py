ad56332df7e2e41db034a19ee42d3d7d4a7733b7
Sean Batzel <romulus108@protonmail.com>
2020-10-21 11:12:16 -0400

    License is set up.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='1.0.0',
     packages=['lib', 'lib.config', 'lib.expand', 'lib.compress', 'test', 'test.config', 'test.expand', 'test.compress'],
     url='https://github.com/Romulus10/sqlcomp',
-    license='GPL',
+    license='GPL v3',
     author='Romulus10',
     author_email='romulus108@protonmail.com',
     description=''
